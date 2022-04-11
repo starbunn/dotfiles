ea4e9e23b925afc41352dce881d30089d76b04a3
ashe <rosascript@gmail.com>
2022-04-11 16:53:49 -0400

    add iosevka config (pyrasevid)

---
+++ pyrasevid/build-fonts.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+cp ./private-build-plans.toml ./font/private-build-plans.toml
+cd font
+npm install
+{
+    npm run build -- ttf::pyrasevid && cp -r dist/pyrasevid ../
+    npm run build -- ttf::pyrasevid-term && cp -r dist/pyrasevid-term ../
+    npm run build -- ttf::pyrasevid-quasi && cp -r dist/pyrasevid-quasi ../
+}
+# cd ..
+# {
+#     for f in ./pyrasev-term/ttf/*.ttf; do
+#         ./nerd-fonts/font-patcher -c -ext ttf -out ./nerdfont/ $f
+#     done
+# }
+
---
+++ pyrasevid/copy-fonts.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+cp -r ./pyrasev-term/ ~/.fonts
+cp -r ./pyrasev/ ~/.fonts
+cp -r ./pyrasev-quasi/ ~/.fonts
+# cp -r ./nerdfont ~/.font
