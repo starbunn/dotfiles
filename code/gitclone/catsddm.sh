7365fe1038f1e8e975f0e76dea15e1edf3851fdd
ashe <rosascript@gmail.com>
2022-04-22 12:59:54 -0400

    automatically move provided sddm config file to sddm directory

--- code/gitclone/catsddm.sh
+++ code/gitclone/catsddm.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 gh repo clone catppuccin/sddm Catppuccin
 sudo mv Catppuccin /usr/share/sddm/themes
+[[ -f ~/.config/sddm_config.conf ]] && cp ~/.config/sddm_config.conf /etc/sddm.conf.d/kde_settings.conf
