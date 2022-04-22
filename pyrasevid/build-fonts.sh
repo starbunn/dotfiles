b3d65977e827c6c0ed3497dfd72042c39456a518
ashe <rosascript@gmail.com>
2022-04-21 22:27:08 -0400

    idk whats being added here but something to do with font building

--- pyrasevid/build-fonts.sh
+++ pyrasevid/build-fonts.sh
@@ -3,9 +3,9 @@ cp ./private-build-plans.toml ./font/private-build-plans.toml
 cd font
 npm install
 {
-    npm run build -- ttf::pyrasevid && cp -r dist/pyrasevid ../
-    npm run build -- ttf::pyrasevid-term && cp -r dist/pyrasevid-term ../
-    npm run build -- ttf::pyrasevid-quasi && cp -r dist/pyrasevid-quasi ../
+    npm run build -- ttf-unhinted::pyrasevid && cp -r dist/pyrasevid ../
+    npm run build -- ttf-unhinted::pyrasevid-term && cp -r dist/pyrasevid-term ../
+    npm run build -- ttf-unhinted::pyrasevid-quasi && cp -r dist/pyrasevid-quasi ../
 }
 # cd ..
 # {
