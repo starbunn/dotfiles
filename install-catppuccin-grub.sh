d5b1c5fb7a0bc7eec026a26d08167ae72d3325d6
ashe <rosascript@gmail.com>
2022-04-22 21:39:58 -0400

    fix gh repo link

--- install-catppuccin-grub.sh
+++ install-catppuccin-grub.sh
@@ -1,4 +1,4 @@
-gh repo clone cattpuccin/grub /tmp/catgrub
+gh repo clone catppuccin/grub /tmp/catgrub
 sudo cp -r /tmp/catgrub/catppuccin-grub-theme /usr/share/grub/themes
 sudo cp -r ~/.config/grubfile /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
