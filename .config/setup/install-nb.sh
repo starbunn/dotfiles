438b994e4be69cb5ae8d9a0db915bd2af3fb5c86
ashe <rosascript@gmail.com>
2022-04-24 01:08:10 -0400

    install completions correctly

--- .config/setup/install-nb.sh
+++ .config/setup/install-nb.sh
@@ -3,4 +3,4 @@ paru -S bat pandoc ripgrep tig w3m nmap
 
 sudo curl -L https://raw.github.com/xwmx/nb/master/nb -o /usr/local/bin/nb &&
     sudo chmod +x /usr/local/bin/nb &&
-    sudo nb completions install
+    sudo nb completions install --download
