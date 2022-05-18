17247db12290455cff8b16f933190783c7027373
ashe <rosascript@gmail.com>
2022-05-18 15:46:40 -0400

    something

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,7 +10,8 @@ polybar-msg cmd quit
 MONITOR=$(polybar -m | tail -1 | sed -e 's/:.*$//g')
 
 # launch bars
-echo "--- launched by $0 ---" | tee -a /tmp/polybar-main.log
-polybar example 2>&1 | tee -a /tmp/polybar-main.log & disown
+log_location="$(xdg-user-dir DOCUMENTS)/logs"
+echo "--- launched by $0 ---" | tee -a "$log_location/polybar.log"
+polybar example 2>&1 | tee -a "$log_location/polybar.log" & disown
 
 echo "launched all bars!"
