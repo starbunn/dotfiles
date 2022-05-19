fed35112bcd64cf6be56dcf213f0b483d8e6abd2
ashe <rosascript@gmail.com>
2022-05-18 20:33:42 -0400

    test font

---
+++ ne/bunni/test-font.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -e "[1m abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890-=!@#\$%^&*()[]{}\|:;'\"\"'.>/?,< \033[0m"
+echo -e "[3m abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890-=!@#\$%^&*()[]{}\|:;'\"\"'.>/?,< \033[0m"
+echo -e "[3m[1m abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890-=!@#\$%^&*()[]{}\|:;'\"\"'.>/?,< \033[0m"
+echo -e " abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890-=!@#\$%^&*()[]{}\|:;'\"\"'.>/?,< \033[0m"
